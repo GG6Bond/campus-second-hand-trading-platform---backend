31268d524e480d8234e8a2890f10dcb272809762
lpz <lpz>
2023-04-18 13:03:14 +0800

    验证码登录

--- src/config/yzm.js
+++ src/config/yzm.js
@@ -2,7 +2,7 @@
 // 生成6位随机数字验证码和过期时间（10分钟）
 function generateVerificationCode() {
     const code = Math.floor(Math.random() * 900000 + 100000).toString();
-    const expirationTime = Date.now() + 5 * 60 * 1000; // 当前时间 + 10分钟
+    const expirationTime = Date.now() + 1 * 60 * 1000; // 当前时间 + 1分钟
     return { code, expirationTime };
 }
 
